ec066fd3c9b55c83f837f7001a51f06214773961
ahtwet12 <anhtuyetdu21@gmail.com>
2024-12-22 14:36:19 +0700

    add featured mugs-product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,27 @@ module.exports = {
         Karlar: ['Karla', 'san-serif']
       },
       colors:{
-         'light-coffee': '#C89F94'
+        
+         'coffee': {
+            50: '#E8D6D0',
+            200: '#C89F94',
+            400: '#A25F4B',
+            600: '#744838',
+         }
       },
       keyframes:{
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          from: {opacity: 0},
+          to: {opacity: 1}
         }
       },
       animation: {
-        'slideDown': 'slideDown .4s ease-in-out',
+        'slideDown': 'slideDown .5s ease-in-out',
+        'fadeIn': 'fadeIn .5s ease-in-out',
       },
       backgroundImage:{
         'slider-bg': 'url("./img/static/bg.jpg") '
